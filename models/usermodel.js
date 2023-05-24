2aee8ae90d4755e8ad3591d5159149d0d09db56b
akshayvpdev <akshayvpofficial@gmail.com>
2023-05-24 22:21:05 +0530

    db updated

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,19 +1,24 @@
-
-
 //import mongoose
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost:27017/Reminderdb",{
-    useNewUrlParser:true
-})
+const mongoose = require("mongoose");
 
-//defining model
-const User= mongoose.model("User",{
-    email:String,
-    name:String,
-    password:String,
-    reminders:[]
-})
+try {
+  mongoose.connect("mongodb://localhost:27017/Reminderdb", {
+    useNewUrlParser: true,
+  });
+  console.log("Connected to MongoDB");
 
-module.exports={
-    User
+  const User = mongoose.model("User", {
+    email: String,
+    name: String,
+    password: String,
+    reminders: [],
+  });
+
+  // Exporting the User model
+  module.exports = {
+    User,
+  };
+} catch (error) {
+  console.error("Error connecting to MongoDB:", error);
 }
