a9268568efe25bba79d810beb31d37f926cb054b
akshayvpdev <akshayvpofficial@gmail.com>
2023-05-24 22:03:26 +0530

    services updated

--- services/user-service.js
+++ services/user-service.js
@@ -4,28 +4,37 @@ const moment = require("moment");
 
 // -------------------------------------------------------------------------------
 const createUser = (email, username, password) => {
-  return usermodel.User.findOne({ email: email }).then((user) => {
-    if (user) {
+  return usermodel.User.findOne({ email: email })
+    .then((user) => {
+      if (user) {
+        return {
+          status: false,
+          message: "Account already exists! Please login.",
+          statusCode: 404,
+        };
+      } else {
+        let user = new usermodel.User({
+          email: email,
+          name: username,
+          password: password,
+          reminders: [],
+        });
+        user.save();
+        return {
+          status: true,
+          message: "Registration completed",
+          statusCode: 201,
+        };
+      }
+    })
+    .catch((error) => {
+      console.log(error);
       return {
         status: false,
-        message: "Account already exist!...... please login",
-        statusCode: 404,
+        message: "An error occurred while creating the user",
+        statusCode: 500,
       };
-    } else {
-      let user = new usermodel.User({
-        email: email,
-        name: username,
-        password: password,
-        reminders: [],
-      });
-      user.save();
-      return {
-        status: true,
-        message: "Registration completed",
-        statusCode: 201,
-      };
-    }
-  });
+    });
 };
 
 // --------------------------------------------------------------------------------
@@ -34,30 +43,39 @@ const loginUser = (email, password) => {
   return usermodel.User.findOne({
     email: email,
     password: password,
-  }).then((res) => {
-    if (res) {
-      currentUser = res.name;
-      currentUserEmail = email;
-      id = res._id;
-      console.log(id + "from loginuser-client");
-      token = jwt.sign({ currentUserEmail: email }, "secretsuperkey1234");
-      return {
-        status: true,
-        message: "Login successfull",
-        statusCode: 200,
-        token,
-        currentUser,
-        currentUserEmail,
-        id,
-      };
-    } else {
+  })
+    .then((res) => {
+      if (res) {
+        currentUser = res.name;
+        currentUserEmail = email;
+        id = res._id;
+        console.log(id + "from loginuser-client");
+        token = jwt.sign({ currentUserEmail: email }, "secretsuperkey1234");
+        return {
+          status: true,
+          message: "Login successful",
+          statusCode: 200,
+          token,
+          currentUser,
+          currentUserEmail,
+          id,
+        };
+      } else {
+        return {
+          status: false,
+          message: "Invalid password or account number",
+          statusCode: 400,
+        };
+      }
+    })
+    .catch((error) => {
+      console.log(error);
       return {
         status: false,
-        message: "Invalid password or account number",
-        statusCode: 400,
+        message: "An error occurred while logging in",
+        statusCode: 500,
       };
-    }
-  });
+    });
 };
 
 // -------------------------------------------------------------------------------------
@@ -65,28 +83,37 @@ const addData = (data, req) => {
   const formattedDate = moment(data.date).format("DD-MM-YYYY");
   return usermodel.User.findOne({
     email: req.currentUserEmail,
-  }).then((res) => {
-    if (res) {
-      const newReminder = {
-        date: formattedDate,
-        description: data.description,
-      };
+  })
+    .then((res) => {
+      if (res) {
+        const newReminder = {
+          date: formattedDate,
+          description: data.description,
+        };
 
-      res.reminders.push(newReminder);
-      res.save();
-      return {
-        status: true,
-        message: "reminder successfully added",
-        statusCode: 200,
-      };
-    } else {
+        res.reminders.push(newReminder);
+        res.save();
+        return {
+          status: true,
+          message: "Reminder successfully added",
+          statusCode: 200,
+        };
+      } else {
+        return {
+          status: false,
+          message: "Not authenticated",
+          statusCode: 400,
+        };
+      }
+    })
+    .catch((error) => {
+      console.log(error);
       return {
         status: false,
-        message: "not autheniticated",
-        statusCode: 400,
+        message: "An error occurred while adding the reminder",
+        statusCode: 500,
       };
-    }
-  });
+    });
 };
 
 // --------------------------------------------------------------------------------------------------
@@ -94,24 +121,34 @@ const addData = (data, req) => {
 const view = (id) => {
   return usermodel.User.findOne({
     _id: id,
-  }).then((res) => {
-    if (res) {
-      console.log(res);
-      return {
-        status: true,
-        message: "get all reninders",
-        data: res.reminders,
-        statusCode: 200,
-      };
-    } else {
+  })
+    .then((res) => {
+      if (res) {
+        console.log(res);
+        return {
+          status: true,
+          message: "Get all reminders",
+          data: res.reminders,
+          statusCode: 200,
+        };
+      } else {
+        return {
+          status: false,
+          message: "Cannot access reminders",
+          statusCode: 400,
+        };
+      }
+    })
+    .catch((error) => {
+      console.log(error);
       return {
         status: false,
-        message: "can not access reminders",
-        statusCode: 400,
+        message: "An error occurred while retrieving the reminders",
+        statusCode: 500,
       };
-    }
-  });
+    });
 };
+
 const remove = (id, req) => {
   return usermodel.User.findOne({
     email: req.currentUserEmail,
